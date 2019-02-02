b9ad7a3fc98e18251b01c476aa8f32518d850958
Daniel Schneider <daschne8@gmail.com>
2019-02-02 01:58:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-
+function getFirstSelector(selector){function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+function nestedTarget(nested, target){
+  return document.querySelector('#nested .target');
+}
+function increaseRankBy(n){
+  const lis = document.querySelectorAll('.ranked-list li');
+  console.log(lis.toString);
+  for(let i = 0; i < lis.length; i++){
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+  }
+}
+/*
+function deepestChild(){
+  const grandNode = document.querySelector('div#grand-node');
+  //list of children elements organized by depth
+  const listByDepth = [[grandNode,0]];
+  //list of next elements to search for children
+  const depth = 0
+  const next = [[grandNode,0];
+  while(next.length>1){
+    depth = next[0][1] + 1;
+    const children = next[0][0].childNodes;
+    for (var child in children) {
+      listByDepth.push([child,depth]);
+      next.push([child,depth]);
+    }
+    next.shift();
+  }
+}
+*/
+function deepestChild(){
+  const grandNode = document.querySelector('div#grand-node');
+  let nextNode = grandNode;
+  while(next.length > 0){
+    if(nextNode.hasChildNodes()){
+      let nextNode = nextNode.firstChild;
+    }else{
+      return nextNode;
+    }
+  }
+}
